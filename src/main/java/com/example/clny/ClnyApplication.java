34b6ce7017d0816f9728e2dee6101ffbde6b2a97
GignacSamuel <samuelgignac@proton.me>
2024-02-18 15:07:11 -0500

    first commit

---
+++ src/main/java/com/example/clny/ClnyApplication.java
@@ -0,0 +1,13 @@
+package com.example.clny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClnyApplication.class, args);
+	}
+
+}
