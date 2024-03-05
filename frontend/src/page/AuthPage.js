4c94c2b8450a70bc0e0d69f408829a7f4879c8a8
GignacSamuel <samuelgignac@proton.me>
2024-03-05 16:16:17 -0500

    started auth view on AuthPage

--- frontend/src/page/AuthPage.js
+++ frontend/src/page/AuthPage.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { createClient } from 'pexels';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs"
 
 function AuthPage() {
-    const [backgroundImageUrl, setBackgroundImageUrl] = useState('');
-    const [opacity, setOpacity] = useState(0);
+    const [backgroundUrl, setBackgroundUrl] = useState('');
+    const [backgroundOpacity, setBackgroundOpacity] = useState(0);
 
     useEffect(() => {
         const client = createClient(process.env.REACT_APP_PEXELS_KEY);
@@ -12,8 +13,8 @@ function AuthPage() {
         client.photos.search({ query, per_page: 1 })
             .then(data => {
                 if (data.photos.length > 0) {
-                    setBackgroundImageUrl(data.photos[0].src.original);
-                    setOpacity(1);
+                    setBackgroundUrl(data.photos[0].src.original);
+                    setBackgroundOpacity(1);
                 }
             })
             .catch(error => {
@@ -23,16 +24,30 @@ function AuthPage() {
 
     return (
         <div
-            className="h-screen"
+            className="h-screen flex justify-center items-center"
             style={{
-                backgroundImage: `url(${backgroundImageUrl})`,
+                backgroundImage: `url(${backgroundUrl})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 transition: 'opacity 3s ease-out',
-                opacity: opacity,
+                opacity: backgroundOpacity,
             }}
         >
-            AuthPage
+            <div className="h-2/3 w-2/3 flex justify-around items-center bg-white rounded-lg shadow">
+                <div className="flex-1 flex justify-center items-center">
+                    <img src="/logo.png" alt="CLNY Logo"/>
+                </div>
+                <div className="flex-1 flex justify-center items-center">
+                    <Tabs defaultValue="login" className="w-[400px]">
+                        <TabsList>
+                            <TabsTrigger value="login">Login</TabsTrigger>
+                            <TabsTrigger value="register">Register</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="login">Login Form</TabsContent>
+                        <TabsContent value="register">Register Form</TabsContent>
+                    </Tabs>
+                </div>
+            </div>
         </div>
     );
 }
