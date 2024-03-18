e9a034b0492cab6053d14bcf198c945eae3f3cf9
GignacSamuel <samuelgignac@proton.me>
2024-03-17 23:02:24 -0400

    added edit profile/banner picture interaction

--- frontend/src/page/ProfilePage.js
+++ frontend/src/page/ProfilePage.js
@@ -4,6 +4,7 @@ import {Button} from "../components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar"
 import {useNavigate} from "react-router-dom";
 import {AuthContext} from "../context/AuthContext";
+import { Pencil } from 'lucide-react';
 
 function ProfilePage() {
     const navigate = useNavigate();
@@ -16,6 +17,16 @@ function ProfilePage() {
 
     const handleProfileClick = () => {
         navigate('/profile');
+    };
+
+    const handleProfilePicChange = (event) => {
+        const file = event.target.files[0];
+        console.log(file);
+    };
+
+    const handleBannerPicChange = (event) => {
+        const file = event.target.files[0];
+        console.log(file);
     };
 
     const Header = () => {
@@ -64,12 +75,24 @@ function ProfilePage() {
     const Profile = () => {
         return (
             <div className="bg-slate-100 m-6 p-6">
-                <div className="bg-cover bg-center bg-slate-200 h-64">
-                    <div className="relative h-full flex items-end justify-start p-6">
-                        <Avatar className="border-4 border-white rounded-full w-32 h-32">
-                            <AvatarImage src="https://github.com/shadcn.png"/>
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
+                <div className="relative bg-cover bg-center bg-slate-200 h-64">
+                    <input id="banner-pic" type="file" accept="image/*" onChange={handleBannerPicChange} className="hidden" />
+                    <label htmlFor="banner-pic" className="absolute bottom-0 right-0 m-4 cursor-pointer">
+                        <div className="p-2 bg-black bg-opacity-50 rounded-full hover:bg-opacity-75 transition-opacity duration-300">
+                            <Pencil color="white" />
+                        </div>
+                    </label>
+                    <div className="absolute bottom-0 left-0 p-6">
+                        <div className="relative group">
+                            <Avatar className="border-4 border-white rounded-full w-32 h-32">
+                                <AvatarImage src="https://github.com/shadcn.png"/>
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <label htmlFor="profile-pic" className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-full cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                <Pencil color="white" />
+                                <input id="profile-pic" type="file" accept="image/*" onChange={handleProfilePicChange} className="absolute inset-0 w-full h-full opacity-0 cursor-pointer" />
+                            </label>
+                        </div>
                     </div>
                 </div>
                 <div className="pt-6">
