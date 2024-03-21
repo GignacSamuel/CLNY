f868266a13a23bb0df09019afb9076b2ab8148d3
GignacSamuel <samuelgignac@proton.me>
2024-03-20 23:50:29 -0400

    added tests for UserService and UserController

--- src/test/java/com/example/clny/controller/UserControllerTest.java
+++ src/test/java/com/example/clny/controller/UserControllerTest.java
@@ -4,9 +4,7 @@ import com.example.clny.dto.CredentialsDTO;
 import com.example.clny.dto.ProfileDTO;
 import com.example.clny.dto.UserDTO;
 import com.example.clny.exception.GlobalExceptionHandler;
-import com.example.clny.exception.custom.EmailAlreadyInUseException;
-import com.example.clny.exception.custom.NoAccountAssociatedWithEmailException;
-import com.example.clny.exception.custom.WrongPasswordException;
+import com.example.clny.exception.custom.*;
 import com.example.clny.filter.JwtAuthenticationFilter;
 import com.example.clny.model.AuthenticationResponse;
 import com.example.clny.service.JwtService;
@@ -20,12 +18,16 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -140,6 +142,160 @@ public class UserControllerTest {
         mockMvc.perform(post("/user/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(payload))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateProfilePictureTest_HappyPath() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        MockMultipartFile file = new MockMultipartFile("file", "test.png", MediaType.IMAGE_PNG_VALUE, "image data".getBytes());
+        String expectedWebPath = "/uploads/profile-pictures/newFileName.png";
+
+        CredentialsDTO credentialsDTO = new CredentialsDTO("email@gmail.com", "password");
+        ProfileDTO profileDTO = new ProfileDTO(expectedWebPath, "bannerPicture", "bio");
+        UserDTO userDTO = new UserDTO("firstName", "lastName", credentialsDTO, profileDTO);
+
+        when(userService.updateProfilePicture(eq(userId), any(MultipartFile.class))).thenReturn(userDTO);
+
+        // Act and Assert
+        mockMvc.perform(multipart("/user/updateProfilePicture/{userId}", userId)
+                        .file(file)
+                        .with(request -> {
+                            request.setMethod("PUT");
+                            return request;
+                        }))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateProfilePictureTest_EmptyFile() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        MockMultipartFile emptyFile = new MockMultipartFile("file", "test.png", MediaType.IMAGE_PNG_VALUE, new byte[0]);
+
+        when(userService.updateProfilePicture(eq(userId), any(MultipartFile.class))).thenThrow(EmptyFileException.class);
+
+        // Act and Assert
+        mockMvc.perform(multipart("/user/updateProfilePicture/{userId}", userId)
+                        .file(emptyFile)
+                        .with(request -> {
+                            request.setMethod("PUT");
+                            return request;
+                        }))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateProfilePictureTest_FileNotImage() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        MockMultipartFile nonImageFile = new MockMultipartFile("file", "test.txt", MediaType.TEXT_PLAIN_VALUE, "text data".getBytes());
+
+        when(userService.updateProfilePicture(eq(userId), any(MultipartFile.class))).thenThrow(FileNotImageException.class);
+
+        // Act and Assert
+        mockMvc.perform(multipart("/user/updateProfilePicture/{userId}", userId)
+                        .file(nonImageFile)
+                        .with(request -> {
+                            request.setMethod("PUT");
+                            return request;
+                        }))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateProfilePictureTest_FileTooLarge() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        MockMultipartFile largeFile = new MockMultipartFile("file", "test.png", MediaType.IMAGE_PNG_VALUE, new byte[1024 * 1024 * 11]);
+
+        when(userService.updateProfilePicture(eq(userId), any(MultipartFile.class))).thenThrow(FileTooLargeException.class);
+
+        // Act and Assert
+        mockMvc.perform(multipart("/user/updateProfilePicture/{userId}", userId)
+                        .file(largeFile)
+                        .with(request -> {
+                            request.setMethod("PUT");
+                            return request;
+                        }))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateBannerPictureTest_HappyPath() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        MockMultipartFile file = new MockMultipartFile("file", "test.png", MediaType.IMAGE_PNG_VALUE, "image data".getBytes());
+        String expectedWebPath = "/uploads/banner-pictures/newFileName.png";
+
+        CredentialsDTO credentialsDTO = new CredentialsDTO("email@gmail.com", "password");
+        ProfileDTO profileDTO = new ProfileDTO("profilePicture", expectedWebPath, "bio");
+        UserDTO userDTO = new UserDTO("firstName", "lastName", credentialsDTO, profileDTO);
+
+        when(userService.updateBannerPicture(eq(userId), any(MultipartFile.class))).thenReturn(userDTO);
+
+        // Act and Assert
+        mockMvc.perform(multipart("/user/updateBannerPicture/{userId}", userId)
+                        .file(file)
+                        .with(request -> {
+                            request.setMethod("PUT");
+                            return request;
+                        }))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateBannerPictureTest_EmptyFile() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        MockMultipartFile emptyFile = new MockMultipartFile("file", "test.png", MediaType.IMAGE_PNG_VALUE, new byte[0]);
+
+        when(userService.updateBannerPicture(eq(userId), any(MultipartFile.class))).thenThrow(EmptyFileException.class);
+
+        // Act and Assert
+        mockMvc.perform(multipart("/user/updateBannerPicture/{userId}", userId)
+                        .file(emptyFile)
+                        .with(request -> {
+                            request.setMethod("PUT");
+                            return request;
+                        }))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateBannerPictureTest_FileNotImage() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        MockMultipartFile nonImageFile = new MockMultipartFile("file", "test.txt", MediaType.TEXT_PLAIN_VALUE, "text data".getBytes());
+
+        when(userService.updateBannerPicture(eq(userId), any(MultipartFile.class))).thenThrow(FileNotImageException.class);
+
+        // Act and Assert
+        mockMvc.perform(multipart("/user/updateBannerPicture/{userId}", userId)
+                        .file(nonImageFile)
+                        .with(request -> {
+                            request.setMethod("PUT");
+                            return request;
+                        }))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateBannerPictureTest_FileTooLarge() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        MockMultipartFile largeFile = new MockMultipartFile("file", "test.png", MediaType.IMAGE_PNG_VALUE, new byte[1024 * 1024 * 11]);
+
+        when(userService.updateBannerPicture(eq(userId), any(MultipartFile.class))).thenThrow(FileTooLargeException.class);
+
+        // Act and Assert
+        mockMvc.perform(multipart("/user/updateBannerPicture/{userId}", userId)
+                        .file(largeFile)
+                        .with(request -> {
+                            request.setMethod("PUT");
+                            return request;
+                        }))
                 .andExpect(status().isBadRequest());
     }
 
--- src/test/java/com/example/clny/service/UserServiceTest.java
+++ src/test/java/com/example/clny/service/UserServiceTest.java
@@ -3,9 +3,7 @@ package com.example.clny.service;
 import com.example.clny.dto.CredentialsDTO;
 import com.example.clny.dto.ProfileDTO;
 import com.example.clny.dto.UserDTO;
-import com.example.clny.exception.custom.EmailAlreadyInUseException;
-import com.example.clny.exception.custom.NoAccountAssociatedWithEmailException;
-import com.example.clny.exception.custom.WrongPasswordException;
+import com.example.clny.exception.custom.*;
 import com.example.clny.mapper.UserMapper;
 import com.example.clny.model.AuthenticationResponse;
 import com.example.clny.model.Credentials;
@@ -22,13 +20,17 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -166,6 +168,226 @@ public class UserServiceTest {
         assertThrows(WrongPasswordException.class, () -> {
             userService.login(credentialsDTO);
         }, "Expected login() to throw WrongPasswordException, but it didn't");
+    }
+
+    @Test
+    void updateProfilePictureTest_HappyPath() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        String expectedWebPath = "/uploads/profile-pictures/newFileName.png";
+        MultipartFile file = mock(MultipartFile.class);
+
+        Credentials credentials = new Credentials("email@gmail.com", "password");
+        Profile profile = new Profile("profilePicture", "bannerPicture", "bio");
+        User user = new User("firstName", "lastName", credentials, profile);
+
+        CredentialsDTO credentialsDTO = new CredentialsDTO("email@gmail.com", "password");
+        ProfileDTO profileDTO = new ProfileDTO(expectedWebPath, "bannerPicture", "bio");
+        UserDTO userDTO = new UserDTO("firstName", "lastName", credentialsDTO, profileDTO);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        when(file.isEmpty()).thenReturn(false);
+        when(file.getContentType()).thenReturn("image/png");
+        when(file.getSize()).thenReturn(1024L); // 1KB
+        when(file.getOriginalFilename()).thenReturn("originalFileName.png");
+        doNothing().when(file).transferTo(any(Path.class));
+
+        when(userMapper.userToUserDTO(any(User.class))).thenReturn(userDTO);
+
+        // Act
+        UserDTO result = userService.updateProfilePicture(userId, file);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(expectedWebPath, result.getProfile().getProfilePicture());
+    }
+
+    @Test
+    void updateProfilePictureTest_UserDoesNotExist() {
+        // Arrange
+        Long userId = 1L;
+        MultipartFile file = mock(MultipartFile.class);
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            userService.updateProfilePicture(userId, file);
+        }, "Expected updateProfilePicture() to throw IllegalArgumentException, but it didn't");
+    }
+
+    @Test
+    void updateProfilePictureTest_EmptyFile() {
+        // Arrange
+        Long userId = 1L;
+        MultipartFile file = mock(MultipartFile.class);
+
+        Credentials credentials = new Credentials("email@gmail.com", "password");
+        Profile profile = new Profile("profilePicture", "bannerPicture", "bio");
+        User user = new User("firstName", "lastName", credentials, profile);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        when(file.isEmpty()).thenReturn(true);
+
+        // Act and Assert
+        assertThrows(EmptyFileException.class, () -> {
+            userService.updateProfilePicture(userId, file);
+        }, "Expected updateProfilePicture() to throw EmptyFileException, but it didn't");
+    }
+
+    @Test
+    void updateProfilePictureTest_FileNotImage() {
+        // Arrange
+        Long userId = 1L;
+        MultipartFile file = mock(MultipartFile.class);
+
+        Credentials credentials = new Credentials("email@gmail.com", "password");
+        Profile profile = new Profile("profilePicture", "bannerPicture", "bio");
+        User user = new User("firstName", "lastName", credentials, profile);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        when(file.isEmpty()).thenReturn(false);
+        when(file.getContentType()).thenReturn("notimage");
+
+        // Act and Assert
+        assertThrows(FileNotImageException.class, () -> {
+            userService.updateProfilePicture(userId, file);
+        }, "Expected updateProfilePicture() to throw FileNotImageException, but it didn't");
+    }
+
+    @Test
+    void updateProfilePictureTest_FileTooLarge() {
+        // Arrange
+        Long userId = 1L;
+        MultipartFile file = mock(MultipartFile.class);
+
+        Credentials credentials = new Credentials("email@gmail.com", "password");
+        Profile profile = new Profile("profilePicture", "bannerPicture", "bio");
+        User user = new User("firstName", "lastName", credentials, profile);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        when(file.isEmpty()).thenReturn(false);
+        when(file.getContentType()).thenReturn("image/png");
+        when(file.getSize()).thenReturn((long) (11 * 1024 * 1024)); // 11MB
+
+        // Act and Assert
+        assertThrows(FileTooLargeException.class, () -> {
+            userService.updateProfilePicture(userId, file);
+        }, "Expected updateProfilePicture() to throw FileTooLargeException, but it didn't");
+    }
+
+    @Test
+    void updateBannerPictureTest_HappyPath() throws Exception {
+        // Arrange
+        Long userId = 1L;
+        String expectedWebPath = "/uploads/banner-pictures/newFileName.png";
+        MultipartFile file = mock(MultipartFile.class);
+
+        Credentials credentials = new Credentials("email@gmail.com", "password");
+        Profile profile = new Profile("profilePicture", "bannerPicture", "bio");
+        User user = new User("firstName", "lastName", credentials, profile);
+
+        CredentialsDTO credentialsDTO = new CredentialsDTO("email@gmail.com", "password");
+        ProfileDTO profileDTO = new ProfileDTO("profilePicture", expectedWebPath, "bio");
+        UserDTO userDTO = new UserDTO("firstName", "lastName", credentialsDTO, profileDTO);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        when(file.isEmpty()).thenReturn(false);
+        when(file.getContentType()).thenReturn("image/png");
+        when(file.getSize()).thenReturn(1024L); // 1KB
+        when(file.getOriginalFilename()).thenReturn("originalFileName.png");
+        doNothing().when(file).transferTo(any(Path.class));
+
+        when(userMapper.userToUserDTO(any(User.class))).thenReturn(userDTO);
+
+        // Act
+        UserDTO result = userService.updateBannerPicture(userId, file);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(expectedWebPath, result.getProfile().getBannerPicture());
+    }
+
+    @Test
+    void updateBannerPictureTest_UserDoesNotExist() {
+        // Arrange
+        Long userId = 1L;
+        MultipartFile file = mock(MultipartFile.class);
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            userService.updateBannerPicture(userId, file);
+        }, "Expected updateBannerPicture() to throw IllegalArgumentException, but it didn't");
+    }
+
+    @Test
+    void updateBannerPictureTest_EmptyFile() {
+        // Arrange
+        Long userId = 1L;
+        MultipartFile file = mock(MultipartFile.class);
+
+        Credentials credentials = new Credentials("email@gmail.com", "password");
+        Profile profile = new Profile("profilePicture", "bannerPicture", "bio");
+        User user = new User("firstName", "lastName", credentials, profile);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        when(file.isEmpty()).thenReturn(true);
+
+        // Act and Assert
+        assertThrows(EmptyFileException.class, () -> {
+            userService.updateBannerPicture(userId, file);
+        }, "Expected updateBannerPicture() to throw EmptyFileException, but it didn't");
+    }
+
+    @Test
+    void updateBannerPictureTest_FileNotImage() {
+        // Arrange
+        Long userId = 1L;
+        MultipartFile file = mock(MultipartFile.class);
+
+        Credentials credentials = new Credentials("email@gmail.com", "password");
+        Profile profile = new Profile("profilePicture", "bannerPicture", "bio");
+        User user = new User("firstName", "lastName", credentials, profile);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        when(file.isEmpty()).thenReturn(false);
+        when(file.getContentType()).thenReturn("notimage");
+
+        // Act and Assert
+        assertThrows(FileNotImageException.class, () -> {
+            userService.updateBannerPicture(userId, file);
+        }, "Expected updateBannerPicture() to throw FileNotImageException, but it didn't");
+    }
+
+    @Test
+    void updateBannerPictureTest_FileTooLarge() {
+        // Arrange
+        Long userId = 1L;
+        MultipartFile file = mock(MultipartFile.class);
+
+        Credentials credentials = new Credentials("email@gmail.com", "password");
+        Profile profile = new Profile("profilePicture", "bannerPicture", "bio");
+        User user = new User("firstName", "lastName", credentials, profile);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        when(file.isEmpty()).thenReturn(false);
+        when(file.getContentType()).thenReturn("image/png");
+        when(file.getSize()).thenReturn((long) (11 * 1024 * 1024)); // 11MB
+
+        // Act and Assert
+        assertThrows(FileTooLargeException.class, () -> {
+            userService.updateBannerPicture(userId, file);
+        }, "Expected updateBannerPicture() to throw FileTooLargeException, but it didn't");
     }
 
 }
